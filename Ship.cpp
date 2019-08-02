c820fa298258c7a76f5313bf0e884125863ddea0
WM <graczyk.mateusz@interia.pl>
2019-08-02 13:58:08 +0200

    add move, getOldPositions and shipIsMoved funcitions; build ship movement

--- Ship.cpp
+++ Ship.cpp
@@ -4,8 +4,38 @@
 
 using std::cout;
 using std::endl;
+using std::cin;
+
+Ship::Ship() {
+	shipPositionX = 0;
+	shipPositionY = 0;
+}
 
 void Ship::showShipsSelection() {
 	cout << "Ships:" << endl;
 	cout << "1.\u25A0 2.\u25A0\u25A0 3.\u25A0\u25A0\u25A0 4.\u25A0\u25A0\u25A0\u25A0 5.\u25A0\u25A0\u25A0\u25A0\u25A0" << endl;
+}
+
+void Ship::setShipsPosition() {
+	shipIsMoved = false; //First think that player doesnt move ship
+
+	//help
+	cout << "'-1' -left/top, '1' -right/bottom, '0' -accept position" << endl;
+	
+	//horizontal move
+	cout << "Horizontal:";
+	cin >> moveX;
+	oldShipPositionX = shipPositionX;
+	if(moveX != 0) {
+		shipPositionX += moveX;
+		shipIsMoved = true;
+	}
+	//vertical move
+	cout << "Vertical:";
+	cin >> moveY;
+	oldShipPositionY = shipPositionY;
+	if(moveY != 0) {
+		shipPositionY += moveY;
+		shipIsMoved = true;
+	}
 }
--- Ship.h
+++ Ship.h
@@ -2,10 +2,19 @@
 
 class Ship {
 public:
-	Ship() = default;
+	Ship();
 	~Ship() = default;
 
 	void showShipsSelection();
+	void setShipsPosition();
+
+	int* getShipPositionX() { return &shipPositionX; }
+	int* getShipPositionY() { return &shipPositionY; }
+	int getOldShipPositionX() { return oldShipPositionX; }
+	int getOldShipPositionY() { return oldShipPositionY; }
+	short getMoveX() { return moveX; }
+	short getMoveY() { return moveY; }
+	bool getShipIsMoved() { return shipIsMoved; }	
 
 	std::vector<int>* getShip1() { return &ship1; }
 	std::vector<int>* getShip2() { return &ship2; }
@@ -13,6 +22,14 @@ public:
 	std::vector<int>* getShip4() { return &ship4; }
 	std::vector<int>* getShip5() { return &ship5; }
 private:                        
+	int shipPositionX;
+        int shipPositionY;
+	int oldShipPositionX;
+	int oldShipPositionY;
+	short moveX;
+	short moveY;
+	bool shipIsMoved;
+
 	std::vector<int> ship1{std::vector<int>(1, 2)}; // '2' is a number which means ship
 	std::vector<int> ship2{std::vector<int>(2, 2)};
 	std::vector<int> ship3{std::vector<int>(3, 2)};
