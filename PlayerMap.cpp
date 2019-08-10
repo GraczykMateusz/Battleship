6c83110c85bb2bb8321db1c9f7cead3656a0a1e7
Mateusz <graczyk.mateusz@interia.pl>
2019-08-10 10:45:23 +0200

    add: separate setShipsOnMap and resetOldShipPosition to smaller functions.

--- PlayerMap.cpp
+++ PlayerMap.cpp
@@ -10,10 +10,65 @@ void PlayerMap::showMapName() {
 	std::cout << "Player's map:" << std::endl;
 }
 
-void PlayerMap::setShipsOnMap(std::vector<int>* ship, int* shipPositionX, int* shipPositionY) {
-	vecMap2D[*shipPositionX][*shipPositionY] = (*ship)[0];
+//---------Reset position--------//
+void PlayerMap::resetOldShip1Position(int oldShipPositionX, int oldShipPositionY) {
+        vecMap2D[oldShipPositionX][oldShipPositionY] = 0; // '0' means free position
 }
 
-void PlayerMap::resetOldShipPosition(int oldShipPositionX, int oldShipPositionY) {
-	vecMap2D[oldShipPositionX][oldShipPositionY] = 0; // '0' means free position
+void PlayerMap::resetOldShip2Position(int oldShipPositionX, int oldShipPositionY) {
+        vecMap2D[oldShipPositionX][oldShipPositionY] = 0; 
+	vecMap2D[oldShipPositionX][oldShipPositionY + 1] = 0;
 }
+
+void PlayerMap::resetOldShip3Position(int oldShipPositionX, int oldShipPositionY) {
+        vecMap2D[oldShipPositionX][oldShipPositionY] = 0;
+	vecMap2D[oldShipPositionX][oldShipPositionY + 1] = 0; 
+	vecMap2D[oldShipPositionX][oldShipPositionY + 2] = 0;
+}
+
+void PlayerMap::resetOldShip4Position(int oldShipPositionX, int oldShipPositionY) {
+        vecMap2D[oldShipPositionX][oldShipPositionY] = 0; 
+	vecMap2D[oldShipPositionX][oldShipPositionY + 1] = 0;
+	vecMap2D[oldShipPositionX][oldShipPositionY + 2] = 0;
+	vecMap2D[oldShipPositionX][oldShipPositionY + 3] = 0;
+}
+
+void PlayerMap::resetOldShip5Position(int oldShipPositionX, int oldShipPositionY) {
+        vecMap2D[oldShipPositionX][oldShipPositionY] = 0; 
+	vecMap2D[oldShipPositionX][oldShipPositionY + 1] = 0;
+	vecMap2D[oldShipPositionX][oldShipPositionY + 2] = 0;
+	vecMap2D[oldShipPositionX][oldShipPositionY + 3] = 0;
+	vecMap2D[oldShipPositionX][oldShipPositionY + 4] = 0;
+}
+
+//---------Set ship into the map---------//
+void PlayerMap::setShip1(std::vector<int>* ship, int* shipPositionX, int* shipPositionY) {
+	vecMap2D[*shipPositionX][*shipPositionY + 0] = (*ship)[0];
+}
+
+void PlayerMap::setShip2(std::vector<int>* ship, int* shipPositionX, int* shipPositionY) {
+	vecMap2D[*shipPositionX][*shipPositionY + 0] = (*ship)[0];
+	vecMap2D[*shipPositionX][*shipPositionY + 1] = (*ship)[1];
+}
+
+void PlayerMap::setShip3(std::vector<int>* ship, int* shipPositionX, int* shipPositionY) {
+	vecMap2D[*shipPositionX][*shipPositionY + 0] = (*ship)[0];
+        vecMap2D[*shipPositionX][*shipPositionY + 1] = (*ship)[1];
+	vecMap2D[*shipPositionX][*shipPositionY + 2] = (*ship)[2];
+}
+
+void PlayerMap::setShip4(std::vector<int>* ship, int* shipPositionX, int* shipPositionY) {
+	vecMap2D[*shipPositionX][*shipPositionY + 0] = (*ship)[0];
+	vecMap2D[*shipPositionX][*shipPositionY + 1] = (*ship)[1];
+	vecMap2D[*shipPositionX][*shipPositionY + 2] = (*ship)[2];
+	vecMap2D[*shipPositionX][*shipPositionY + 3] = (*ship)[3];
+}
+
+void PlayerMap::setShip5(std::vector<int>* ship, int* shipPositionX, int* shipPositionY) {
+	vecMap2D[*shipPositionX][*shipPositionY + 0] = (*ship)[0];
+	vecMap2D[*shipPositionX][*shipPositionY + 1] = (*ship)[1];
+	vecMap2D[*shipPositionX][*shipPositionY + 2] = (*ship)[2];
+	vecMap2D[*shipPositionX][*shipPositionY + 3] = (*ship)[3];
+	vecMap2D[*shipPositionX][*shipPositionY + 4] = (*ship)[4];
+}
+
--- PlayerMap.h
+++ PlayerMap.h
@@ -8,6 +8,19 @@ public:
 	~PlayerMap() = default;
 	
 	void showMapName();
-	void setShipsOnMap(std::vector<int>* ship, int* shipPositionX, int* shipPositionY);
-	void resetOldShipPosition(int oldShipPositionX, int oldShipPositionY);
+
+	//reset old ship position
+	void resetOldShip1Position(int oldShipPositionX, int oldShipPositionY);
+	void resetOldShip2Position(int oldShipPositionX, int oldShipPositionY);
+	void resetOldShip3Position(int oldShipPositionX, int oldShipPositionY);
+	void resetOldShip4Position(int oldShipPositionX, int oldShipPositionY);
+	void resetOldShip5Position(int oldShipPositionX, int oldShipPositionY);
+
+	//Set ship into the map
+	void setShip1(std::vector<int>* ship, int* shipPositionX, int* shipPositionY);
+	void setShip2(std::vector<int>* ship, int* shipPositionX, int* shipPositionY);
+	void setShip3(std::vector<int>* ship, int* shipPositionX, int* shipPositionY);
+	void setShip4(std::vector<int>* ship, int* shipPositionX, int* shipPositionY);
+	void setShip5(std::vector<int>* ship, int* shipPositionX, int* shipPositionY);
+
 };
