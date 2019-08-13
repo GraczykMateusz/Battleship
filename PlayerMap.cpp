dbd3f73d749049940045852ce204b2950c71e880
Mateusz <graczyk.mateusz@interia.pl>
2019-08-13 09:14:29 +0200

    fix: delete wrong position issue

--- PlayerMap.cpp
+++ PlayerMap.cpp
@@ -1,5 +1,6 @@
 #include "PlayerMap.h"
 
+#include <algorithm>
 #include <iostream>
 
 PlayerMap::PlayerMap() {
@@ -12,22 +13,30 @@ void PlayerMap::showMapName() {
 
 void PlayerMap::resetOldShipPosition(std::vector<int>* ship, bool shipIsRotated, int oldShipPositionX, int oldShipPositionY) {
 	if(!shipIsRotated) {
-		for(int i = 0; i <= (*ship).size(); i++)
+		for(int i = 0; i < (*ship).size(); i++)
 		        vecMap2D[oldShipPositionX][oldShipPositionY + i] = 0; // '0' means free position
 	}
 	else {
-		for(int i = 0; i <= (*ship).size(); i++)
+		for(int i = 0; i < (*ship).size(); i++)
                         vecMap2D[oldShipPositionX + i][oldShipPositionY] = 0;
 	}
 }
 
 void PlayerMap::setShip(std::vector<int>* ship, bool shipIsRotated, int* shipPositionX, int* shipPositionY) {
 	if(!shipIsRotated) {
-		for(int i = 0; i <= (*ship).size(); i++)
+		for(int i = 0; i < (*ship).size(); i++)
 			vecMap2D[*shipPositionX][*shipPositionY + i] = (*ship)[i];
 	}
 	else {
-		for(int i = 0; i <= (*ship).size(); i++)
+		for(int i = 0; i < (*ship).size(); i++)
                         vecMap2D[*shipPositionX + i][*shipPositionY] = (*ship)[i];
 	}
+}
+
+void PlayerMap::shipIsOnMap(short shipNumber) {
+	std::cout << vecShipOnMap.size() << std::endl;
+}
+
+bool PlayerMap::isShipOnMap(short shipNumber) {
+	return std::binary_search(vecShipOnMap.begin(), vecShipOnMap.end(), shipNumber);
 }
