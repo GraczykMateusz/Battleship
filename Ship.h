b78c15fb5ec45fd6516d422b70f5b61e8337b718
Mateusz <graczyk.mateusz@interia.pl>
2019-08-13 09:03:29 +0200

    refactor: delete shipsOnMapCount, add comments

--- Ship.h
+++ Ship.h
@@ -5,46 +5,57 @@ public:
 	Ship();
 	~Ship() = default;
 
+	// Menu
 	void showShipsSelection();
-	void setShipsPosition(std::vector<int>* ship, unsigned int widthMax, unsigned int heightMax);
 
+	// New position
 	int* getShipPositionX() { return &shipPositionX; }
 	int* getShipPositionY() { return &shipPositionY; }
+	void setShipsPosition(std::vector<int>* ship, unsigned int widthMax, unsigned int heightMax);
 	void resetPosition();	
 
+	// Old position
 	int getOldShipPositionX() { return oldShipPositionX; }
 	int getOldShipPositionY() { return oldShipPositionY; }
 
+	// Move
 	short getMoveX() { return moveX; }
 	short getMoveY() { return moveY; }
 
+	// Move settings
 	bool getShipIsMoved() { return shipIsMoved; }	
 	bool getShipIsRotated() { return shipIsRotated; }
 	
-	short getShipsOnMapCount() { return shipsOnMapCount; }
+	// Ships count
 	short getShipMaxCount() { return shipMaxCount; }
-
+	
+	// Ships
 	std::vector<int>* getShip1() { return &ship1; }
 	std::vector<int>* getShip2() { return &ship2; }
 	std::vector<int>* getShip3() { return &ship3; }
 	std::vector<int>* getShip4() { return &ship4; }
 	std::vector<int>* getShip5() { return &ship5; }
-private:                        
+private:
+	// New position                        
 	int shipPositionX;
         int shipPositionY;
 
+	// Old position
 	int oldShipPositionX;
 	int oldShipPositionY;
 
+	// Move
 	short moveX;
 	short moveY;
 
+	// Move settings
 	bool shipIsMoved;
 	bool shipIsRotated;
 
-	short shipsOnMapCount;
+	// Ships count
 	const short shipMaxCount = 5;
 
+	// Ships
 	std::vector<int> ship1{std::vector<int>(1, 2)}; // '2' is a number which means ship
 	std::vector<int> ship2{std::vector<int>(2, 2)};
 	std::vector<int> ship3{std::vector<int>(3, 2)};
