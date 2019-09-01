d0d1f729c8e7838d21ec1a0362e3fbd6691c18e4
Mateusz <graczyk.mateusz@interia.pl>
2019-09-01 22:33:08 +0200

    add: getVecMap2D function

--- Map.h
+++ Map.h
@@ -10,6 +10,7 @@ public:
 	static unsigned int* getMapSizeWidth();
 	void setVecMap2D();
 	void showMap();
+	std::vector<std::vector<int>>* getVecMap2D() { return &vecMap2D; }
 protected:
 	static unsigned int mapSizeHeight;
 	static unsigned int mapSizeWidth;
