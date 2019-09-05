c1db709734091b194530759252bf8a5feb531fe4
Mateusz <graczyk.mateusz@interia.pl>
2019-09-05 10:15:26 +0200

    add: protection against rotate ship when cannot

--- Ship.cpp
+++ Ship.cpp
@@ -18,50 +18,52 @@ void Ship::showShipsSelection() {
 	cout << "1.\u25A0 2.\u25A0\u25A0 3.\u25A0\u25A0\u25A0 4.\u25A0\u25A0\u25A0\u25A0 5.\u25A0\u25A0\u25A0\u25A0\u25A0" << endl;
 }
 
-void Ship::setShipsPosition(std::vector<int>* ship, unsigned int widthMax, unsigned int heightMax, std::vector<std::vector<int>>* vecMap2D) {
+void Ship::setShipsPosition(std::vector<int>* ship, unsigned int& widthMax, unsigned int& heightMax, std::vector<std::vector<int>>* vecMap2D) {
 	shipIsMoved = false; // First think that player doesnt move ship
 
 	// help
 	cout << "'-1' -left/top, '1' -right/bottom, '2' -rotate (only in horizontal option), '0' -accept position (do this twice)" << endl;
 	
-	// protection against set ship on the ship
-	do {	
-		// horizontal move (X)
-		if(!shipIsRotated) {
-			do { // protection against wrong input (go outside the map) [shipIsntRotated]
-				cout << "Horizontal:";
-				cin >> moveX;
-			} while(shipPositionX + moveX < 0 || shipPositionX + moveX >= widthMax || moveX > 2 || moveX < -1);
-	
-			// set old and new position (X)
-			if(moveX != 2) {
-				setPositionX();
-			} 
-			else {
-				if(checkIfShipCanRotateX(ship, widthMax)) {
-					shipIsMoved = true;
-					shipIsRotated = true;
+	do { // protection against set ship on the ship
+		do { // protection against rotate when cannot
+			// horizontal move (X)
+			if(!shipIsRotated) {
+				do { // protection against wrong input (go outside the map) [shipIsntRotated]
+					cout << "Horizontal:";
+					cin >> moveX;
+				} while(shipPositionX + moveX < 0 || (shipPositionX + moveX >= widthMax && moveX != 2) || moveX > 2 || moveX < -1);
+		
+				// set old and new position (X)
+				if(moveX != 2) {
+					setPositionX();
+				} 
+				else {
+					if(checkIfShipCanRotateX(ship, widthMax)) {
+						shipIsMoved = true;
+						shipIsRotated = true;
+					}
 				}
 			}
-		}
-		else { // horizontal move (X) ROTATED SHIP
-			do { // protection against wrong input (go outside the map) [shipIsRotated]
-                                cout << "Horizontal:";
-                                cin >> moveX;
-                        } while(shipPositionX + moveX < 0 || shipPositionX + (*ship).size() + moveX > widthMax || moveX > 2 || moveX < -1);
-			
-                        // set old and new position (X)
-			if(moveX != 2) {
-                                setPositionX();
-                        } 
-                        else {
-                                if(checkIfShipCanRotateY(ship, heightMax)) {
-                        	        shipIsMoved = true;
-					shipIsRotated = false;
-				}
-                        }
-
-		}	
+			else { // horizontal move (X) ROTATED SHIP
+				do { // protection against wrong input (go outside the map) [shipIsRotated]
+	                                cout << "Horizontal:";
+	                                cin >> moveX;
+	                        } while(shipPositionX + moveX < 0 || (shipPositionX + (*ship).size() + moveX > widthMax && moveX != 2) || moveX > 2 || moveX < -1);
+				
+	                        // set old and new position (X)
+				if(moveX != 2) {
+	                                setPositionX();
+	                        } 
+	                        else {
+	                                if(checkIfShipCanRotateY(ship, heightMax)) {
+						shipIsMoved = true;
+						shipIsRotated = false;
+					}
+	                        }
+	
+			}
+		} while((!checkIfShipCanRotateX(ship, widthMax) || !checkIfShipCanRotateY(ship, heightMax)) && moveX == 2);	
+		
 		// vertical move (Y)
 		if(moveX != 2) {
 			if(!shipIsRotated) {
@@ -86,8 +88,8 @@ void Ship::setShipsPosition(std::vector<int>* ship, unsigned int widthMax, unsig
 	} while(checkIfShipIsOnShip(widthMax, heightMax, vecMap2D) && moveX == 0 && moveY == 0);
 }
 
-bool Ship::checkIfShipCanRotateX(std::vector<int>* ship, unsigned int widthMax) {
-	if(shipPositionX + (*ship).size() < widthMax) {
+bool Ship::checkIfShipCanRotateX(std::vector<int>* ship, unsigned int& widthMax) {
+	if(shipPositionX + (*ship).size() <= widthMax) {
 		return true;
 	}
 	else {
@@ -95,8 +97,8 @@ bool Ship::checkIfShipCanRotateX(std::vector<int>* ship, unsigned int widthMax) 
 	}
 }
 
-bool Ship::checkIfShipCanRotateY(std::vector<int>* ship, unsigned int heightMax) {
-        if(shipPositionY + (*ship).size() < heightMax) {
+bool Ship::checkIfShipCanRotateY(std::vector<int>* ship, unsigned int& heightMax) {
+        if(shipPositionY + (*ship).size() <= heightMax) {
                 return true;
         }
         else {
@@ -104,7 +106,7 @@ bool Ship::checkIfShipCanRotateY(std::vector<int>* ship, unsigned int heightMax)
         }
 }
 
-bool Ship::checkIfShipIsOnShip(unsigned int widthMax, unsigned int heightMax, std::vector<std::vector<int>>* vecMap2D) {
+bool Ship::checkIfShipIsOnShip(unsigned int& widthMax, unsigned int& heightMax, std::vector<std::vector<int>>* vecMap2D) {
 	for(int i = 0; i < widthMax; i++) {
 		for(int j = 0; j < heightMax; j++) {
 			if((*vecMap2D)[i][j] == 4)
