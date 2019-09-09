20fefcb8bb7af95aba42385c96287f8b03eb4a35
Mateusz <graczyk.mateusz@interia.pl>
2019-09-09 20:28:19 +0200

    add: functions needed to choose random position

--- Ai.h
+++ Ai.h
@@ -1,11 +1,17 @@
 #include <string>
+#include <vector>
 
 class Ai {
 public:
-	Ai();
+	Ai() = default;
 	~Ai() = default;
 
 	static std::string* getDifficultyLevel();
+	void setPosition(std::vector<int>* ship, unsigned int& widthMax, unsigned int& heightMax, std::vector<std::vector<int>>* vecMap2D);
+	std::vector<int>* getIsRotateVec() { return &isRotateVec; }
+	std::vector<int>* getBotShipPosition() { return &botShipPosition; }
 private:
 	static std::string difficultyLevel;
+	std::vector<int> isRotateVec;
+	std::vector<int> botShipPosition; 
 };
