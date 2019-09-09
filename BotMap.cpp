6caebc62514a41a2b3cc18e23e7b06b135e44803
Mateusz <graczyk.mateusz@interia.pl>
2019-09-09 20:29:09 +0200

    add: set first ship position on the bot map

--- BotMap.cpp
+++ BotMap.cpp
@@ -10,3 +10,19 @@ void BotMap::showMapName() {
         std::cout << "Bot's map:" << std::endl;
 }
 
+void BotMap::setShip(std::vector<int>* botShipPosition, std::vector<int>* isRotateVec) {
+	//Ship5
+	vecMap2D[(*botShipPosition)[0]][(*botShipPosition)[1]] += 2;
+	
+	//Ship4
+	vecMap2D[(*botShipPosition)[2]][(*botShipPosition)[3]] += 2;
+
+	//Ship3
+	vecMap2D[(*botShipPosition)[4]][(*botShipPosition)[5]] += 2;
+
+	//Ship2
+	vecMap2D[(*botShipPosition)[6]][(*botShipPosition)[7]] += 2;
+
+	//Ship1
+	vecMap2D[(*botShipPosition)[8]][(*botShipPosition)[9]] += 2;
+}
