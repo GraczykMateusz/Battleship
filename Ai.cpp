8e9567de407bc829d9aca0a71ede730b3b7b2786
Mateusz <graczyk.mateusz@interia.pl>
2019-09-10 16:30:40 +0200

    fix: set ship on the ship

--- Ai.cpp
+++ Ai.cpp
@@ -17,25 +17,37 @@ void Ai::setPosition(std::vector<int>* ship, unsigned int& widthMax, unsigned in
 	std::uniform_int_distribution<> disY(0, heightMax);
 	std::bernoulli_distribution randBool(0.5);
 	do {
+		short counter = 0;
 		bool isRotate = randBool(gen);		
 
 		int positionX = disX(gen);
 	        int positionY = disY(gen);
 
-		if(positionX + (*ship).size() < widthMax && !isRotate)
-			if(positionY < heightMax) {
-				isRotateVec.push_back(isRotate);
-				botShipPosition.push_back(positionX);
-				botShipPosition.push_back(positionY);
-				isSet = true;
-			}	
+		if(positionX + (*ship).size() < widthMax && isRotate)
+			if(positionY < heightMax)
+				for(int i = 0; i < (*ship).size(); i++) {
+					if((*vecMap2D)[positionX + i][positionY] == 0) {
+						counter++;
+						if(counter == (*ship).size()) {
+							isRotateVec.push_back(isRotate);
+							botShipPosition.push_back(positionX);
+							botShipPosition.push_back(positionY);
+							isSet = true;
+						}
+					}
+				}	
 		if(positionX < widthMax)
-			if(positionY + (*ship).size() < heightMax && isRotate) {
-				isRotateVec.push_back(isRotate);
-				botShipPosition.push_back(positionX);
-				botShipPosition.push_back(positionY);
-				isSet = true;
-			}
-				
+			if(positionY + (*ship).size() < heightMax && !isRotate)
+				for(int j = 0; j < (*ship).size(); j++) {
+					if((*vecMap2D)[positionX][positionY + j] == 0) {
+						counter++;
+						if(counter == (*ship).size()) {
+							isRotateVec.push_back(isRotate);
+							botShipPosition.push_back(positionX);
+							botShipPosition.push_back(positionY);
+							isSet = true;
+						}
+				}
+			}			
 	} while(!isSet);
 }
