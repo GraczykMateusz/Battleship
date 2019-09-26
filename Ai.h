1d2de93f59f22d134d453b676c6bc0e1fbcccc3d
Mateusz <graczyk.mateusz@interia.pl>
2019-09-26 11:14:33 +0200

    add: change difficulty level functions

--- Ai.h
+++ Ai.h
@@ -8,15 +8,25 @@ public:
 
 	static std::string* getDifficultyLevel();
 	void setPosition(std::vector<int>* ship, unsigned int& widthMax, unsigned int& heightMax, std::vector<std::vector<int>>* vecMap2D);
-	std::vector<int>* randomHit(unsigned int widthMax, unsigned int heightMax);
+
+	std::vector<int>* randomHit(std::vector<std::vector<int>>* playerMap, unsigned int& widthMax, unsigned int& heightMax);
 	std::vector<int>* getIsRotateVec() { return &isRotateVec; }
 	std::vector<int>* getBotShipPosition() { return &botShipPosition; }
 
 private:
 	static std::string difficultyLevel;
+	void upgradeHits(std::vector<std::vector<int>>* playerMap, unsigned int& widthMax, unsigned int& heightMax);
+	bool changeHits = true;
+
+	void randomKnownFreeArea(int knownArea);
+
 	std::vector<int> isRotateVec;
 	std::vector<int> botShipPosition; 
 	std::vector<int> randHit;
+
+	std::vector<int> freeAreaX;
+	std::vector<int> freeAreaY;
+
 	std::vector<int> usedHitX;
 	std::vector<int> usedHitY;
 };
